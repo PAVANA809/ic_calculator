396754b9f5e66e2348f6ea87469a54e919ed990e
PAVANA809 <pavanhapavan@gmail.com>
2022-03-10 18:18:38 +0530

    initial

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function calculate(){
     document.getElementById("buy_total").innerHTML = buytotal;
 
 }
+
+
+function sellcalculate(){
+    var price = parseFloat(document.getElementById("price").value);
+    var quantity = parseFloat(document.getElementById("quantity").value);
+    var buycharge = (price * quantity * 0.2) / 100;
+    var buytotal = price * quantity + buycharge;
+    var sellprice = parseFloat(document.getElementById("sell_price").value);
+    var sell_total = quantity * sellprice;
+    var sell_charge = (sell_total * 0.2) / 100;
+    var total_sell = sell_total - sell_charge;
+    document.getElementById("sell_total").innerHTML = sell_total;
+    document.getElementById("sell_charge").innerHTML = sell_charge;
+    document.getElementById("total_sell_price").innerHTML = total_sell;
+
+
+    var revenue = total_sell - buytotal;
+    document.getElementById("revenue").innerHTML = revenue;
+}
