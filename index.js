1c159965d2dd4b2da7ceeffa490127c05a8e1d36
PAVANA809 <pavanhapavan@gmail.com>
2023-12-31 21:02:54 +0530

    31-12-23 added name and fee details.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function calculate(){
+let platformFee = 0.472;
+let TDSFee = 0.01;
+function calculate() {
     var price = parseFloat(document.getElementById("price").value);
     var quantity = parseFloat(document.getElementById("quantity").value);
-    var buycharge = (price * quantity * 0.472) / 100;
+    var buycharge = (price * quantity * platformFee) / 100;
     var buytotal = (price * quantity) + buycharge;
     document.getElementById("total").innerHTML = quantity * price;
     document.getElementById("buy_charge").innerHTML = buycharge;
@@ -13,12 +15,12 @@ function calculate(){
 function sellcalculate(){
     var price = parseFloat(document.getElementById("price").value);
     var quantity = parseFloat(document.getElementById("quantity").value);
-    var buycharge = (price * quantity * 0.472) / 100;
+    var buycharge = (price * quantity * platformFee) / 100;
     var buytotal = price * quantity + buycharge;
     var sellprice = parseFloat(document.getElementById("sell_price").value);
     var sell_total = quantity * sellprice;
-    var sell_charge = (sell_total * 0.472) / 100;
-    var tds = (sell_total - sell_charge) * 0.01;
+    var sell_charge = (sell_total * platformFee) / 100;
+    var tds = (sell_total - sell_charge) * TDSFee;
     var total_sell = sell_total - sell_charge - tds;
     
     document.getElementById("sell_total").innerHTML = sell_total;
